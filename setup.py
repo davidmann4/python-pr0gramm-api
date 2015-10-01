ca52543149ea6bc304e979d7ec1aed35494b3e1a
David Mann <daaavid.mann@gmail.com>
2015-10-02 00:09:56 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-pr0gramm-api',
+      version='0.1',
+      description='python wrapper for the pr0gramm API',
+      author='David Mann',
+      author_email='daaavid.mann@gmail.com',
+      url='https://github.com/davidmann4/python-pr0gramm-api/',
+      packages=['distutils', 'distutils.command'],
+     )
