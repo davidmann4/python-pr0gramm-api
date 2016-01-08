e9a820639a86b03f215eb55e70c872266e808c05
David Mann <daaavid.mann@gmail.com>
2016-01-08 21:55:44 +0100

    filter settings and item meta infos

--- pr0gramm.py
+++ pr0gramm.py
@@ -4,19 +4,41 @@ import requests
 class Api:
     def __init__(self):
         self.server = "http://pr0gramm.com/api/"
+        self.flags = 0
 
+    def enableNSFW(self):
+        self.flags += 2
+                
+    def disableNSFW(self):
+        self.flags -= 2
+        
+    def enableSFW(self):
+        self.flags += 1
+        
+    def disableSFW(self):
+        self.flags -= 1
 
     def search(self, q):
         # Set up the arguments for the REST call.
         args = ({
-            'older': 104574,
-            'flags': 1,
+            #'older': 104574,
+            'flags': self.flags,
             'promoted': 1,
             'tags': q
         })
+        
         # Make the request and verify success.
         url = self.server + 'items/get'
         response = requests.get(url, params= args)
         response.raise_for_status()
         return response.json()["items"]
 
+    def info(self, id):
+        args = ({
+             'itemId': id
+        })
+        
+        url = self.server + 'items/info'
+        response = requests.get(url, params= args)
+        response.raise_for_status()
+        return response.json()
