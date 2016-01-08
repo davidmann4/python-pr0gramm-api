5a9f36f6f90c17dc2e79592f9196732e617c4f3b
David Mann <daaavid.mann@gmail.com>
2016-01-08 23:45:44 +0100

    fixed test: test_API_search_find_nothing
    there is some awww nixname content - replaced it with a random uuid

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_API_search_find_nothing(self):
         pr0_api = pr0gramm.Api()
-        result = pr0_api.search("awww nixname")
+        result = pr0_api.search("awww 1ffe5bfe-2af6-456e-bb43-0595e7792121")
         self.assertTrue(len(result)==0)
 
 
