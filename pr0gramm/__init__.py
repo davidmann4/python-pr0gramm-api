f5298c4ec27179364a9190382471ddb3d3e9ad30
David Mann <daaavid.mann@gmail.com>
2016-01-09 12:19:27 +0100

    setup.py and packaging

---
+++ pr0gramm/__init__.py
@@ -0,0 +1,5 @@
+
+__author__ = 'David Mann <daaavid.mann@gmail.com>'
+__version__ = '0.4.0'
+
+from .pr0gramm import Api
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+__author__ = 'David Mann <daaavid.mann@gmail.com>'
+__version__ = '0.4.0'
+
 
 setup(name='python-pr0gramm-api',
-      version='0.1',
+      version=__version__,
       description='python wrapper for the pr0gramm API',
       author='David Mann',
       author_email='daaavid.mann@gmail.com',
       url='https://github.com/davidmann4/python-pr0gramm-api/',
-      packages=find_packages(),
+      include_package_data=True,
+      packages=['pr0gramm']      
      )
